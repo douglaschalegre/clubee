d59bb3ef83e2bd8c25495f14205604db5fd0f17e
douglaschalegre <douglas.chalegre@gmail.com>
2026-02-01 22:40:39 -0300

    Switch to modern typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        {/* Google Fonts: Fraunces (display) + DM Sans (body) */}
+        {/* Google Fonts: Sora (display) + Manrope (body) */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;1,9..40,400&family=Fraunces:ital,opsz,wght@0,9..144,400;0,9..144,500;0,9..144,600;0,9..144,700;1,9..144,400&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&family=Sora:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </head>
